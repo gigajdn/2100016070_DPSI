2d9eebdb768b6f3485d376ffb630af5acf235891
alffyyan <alfian2100016070@webmail.uad.ac.id>
2024-07-26 04:43:48 +0700

    Accept current changes and merge remote changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 
-<<<<<<< HEAD
 //authMiddleware digunakan untuk memastikan bahwa pengguna telah melakukan authentifikasi dengan login
-=======
->>>>>>> b8b58a706df2f8d745be7c0f91f1daca498af4e6
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authMiddleware, authController.logout);
