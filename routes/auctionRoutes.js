85568a2b9f5ffdef9e63577202d93adac57d806d
alffyyan <alfian2100016070@webmail.uad.ac.id>
2024-07-25 17:44:05 +0700

    update

--- routes/auctionRoutes.js
+++ routes/auctionRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const auctionController = require('../controllers/auctionController');
-const { authMiddleware } = require('../middlewares/authMiddleware');
+const { authMiddleware, employeeMiddleware } = require('../middlewares/authMiddleware');
 
 //authMiddleware digunakan untuk memastikan bahwa pengguna telah melakukan authentifikasi dengan login
-router.post('/auctions', authMiddleware, auctionController.createAuction);
+//adminMiddleware merupakan pengecekan lebih lanjut dengan role yang ada pada pengguna (employee)
+router.post('/auctions', authMiddleware, employeeMiddleware, auctionController.createAuction);
 router.get('/auctions', authMiddleware, auctionController.getAuctions);
 router.get('/auctions/:id', authMiddleware, auctionController.getAuctionById);
 router.put('/auctions/:id', authMiddleware, auctionController.updateAuction);
-router.delete('/auctions/:id', authMiddleware, auctionController.deleteAuction);
-router.post('/auctions/start/:id', authMiddleware, auctionController.startAuction);
-router.post('/auctions/close/:id', authMiddleware, auctionController.closeAuction);
-router.post('/auctions/cancel/:id', authMiddleware, auctionController.cancelAuction);
-router.post('/auctions/confirmReceipt/:id', authMiddleware, auctionController.confirmReceipt);
+router.delete('/auctions/:id', authMiddleware, employeeMiddleware, auctionController.deleteAuction);
+router.post('/auctions/start/:id', authMiddleware, employeeMiddleware, auctionController.startAuction);
+router.post('/auctions/close/:id', authMiddleware, employeeMiddleware, auctionController.closeAuction);
+router.post('/auctions/cancel/:id', authMiddleware, employeeMiddleware, auctionController.cancelAuction);
+router.post('/auctions/confirmReceipt/:id', authMiddleware, employeeMiddleware, auctionController.confirmReceipt);
 
 module.exports = router;
